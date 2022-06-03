6ea2cff8cd6c5a96a5be1be418f69643d019220a
Cinthia Ramos <cinthia24027@gmail.com>
2022-06-02 21:19:45 -0500

    deploy-12

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ const { conn } = require('./src/db.js');
 //   });
 // });
 conn.sync({ force: true}).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 }); 
